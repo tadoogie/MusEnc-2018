695d2c1889aa6d4b50f971b9da582cefd618eb49
tadoogie <tim.duguid@glasgow.ac.uk>
2018-11-02 21:36:15 +0000

    Add psalm

--- js/index.js
+++ js/index.js
@@ -20,4 +20,4 @@ options = {
 vrvToolkit.setOptions(options)
 
 /* Show the score */
-showScore('score', 'data/example.mei') // <-- CHANGE THIS TO YOUR FILE
+showScore('score', 'data/Ps19.mei') // <-- CHANGE THIS TO YOUR FILE
