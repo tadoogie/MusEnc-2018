23997f07e0c6ef09680ef01c47e6b36d6ae03989
tadoogie <tim.duguid@glasgow.ac.uk>
2018-11-13 12:59:26 +0000

    adjust zoom

--- js/index-unforgiven.js
+++ js/index-unforgiven.js
@@ -14,7 +14,7 @@ var defaultHeight = document.documentElement.clientHeight - contentHeight
 var defaultWidth = document.documentElement.clientWidth - 50
 options = {
     pageHeight: defaultHeight * 100 / zoom,
-    pageWidth: defaultWidth * 100 / zoom,
+    pageWidth: defaultWidth * 90 / zoom,
     scale: zoom
 }
 vrvToolkit.setOptions(options)
