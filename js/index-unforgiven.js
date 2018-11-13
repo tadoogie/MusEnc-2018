631d7181062d5b3f72f3bd04cd1f74d5380c6653
tadoogie <tim.duguid@glasgow.ac.uk>
2018-11-13 13:10:05 +0000

    adjust height

--- js/index-unforgiven.js
+++ js/index-unforgiven.js
@@ -6,7 +6,7 @@ var vrvToolkit = new verovio.toolkit()
 /* Setup Verovio - adjust the options below */
 var zoom = 60
 var content = document.getElementById('content')
-var contentHeight = 0
+var contentHeight = 50
 if (content) {
   contentHeight = content.offsetHeight
 }
